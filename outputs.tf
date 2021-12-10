a6c5b0cab515a27e5254f1855084e0b067dfab53
Viet Nguyen <nguyentrungviet04082001@gmail.com>
2021-12-10 18:03:00 +0100

    First commit

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,19 @@ output "kubernetes_cluster_host" {
   value       = google_container_cluster.primary.endpoint
   description = "GKE Cluster Host"
 }
+
+output "kubernetes_client_certificate" {
+  value = google_container_cluster.primary.master_auth[0].client_certificate
+}
+
+output "kubernetes_client_key" {
+  value = google_container_cluster.primary.master_auth[0].client_key
+}
+
+output "kubernetes_username" {
+  value = google_container_cluster.primary.master_auth[0].username
+}
+
+output "kubernetes_password" {
+  value = google_container_cluster.primary.master_auth[0].password
+}
