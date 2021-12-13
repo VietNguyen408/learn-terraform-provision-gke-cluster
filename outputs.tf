17ff121ed856d2afcbe43f707ae2e031cf41b0c6
Viet Nguyen <nguyentrungviet04082001@gmail.com>
2021-12-13 12:19:13 +0100

    Update

--- outputs.tf
+++ outputs.tf
@@ -18,6 +18,10 @@ output "kubernetes_cluster_host" {
   description = "GKE Cluster Host"
 }
 
+output "kubernetes_cluster_ca_certificate" {
+  value = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+}
+
 output "kubernetes_client_certificate" {
   value = google_container_cluster.primary.master_auth[0].client_certificate
 }
