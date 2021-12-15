77b8f22d2720bc2f7e3604195e9ac7d142d3d5d6
Viet Nguyen <nguyentrungviet04082001@gmail.com>
2021-12-15 11:50:03 +0100

    add new file

--- vpc.tf
+++ vpc.tf
@@ -28,7 +28,7 @@ resource "google_compute_network" "vpc" {
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
+  name          = "${var.project_id}--subnet"
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
