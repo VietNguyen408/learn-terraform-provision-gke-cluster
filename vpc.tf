869c02c18f8a9debdc43ab31b746578bc7791d88
Viet Nguyen <nguyentrungviet04082001@gmail.com>
2021-12-16 11:19:06 +0100

    update

--- vpc.tf
+++ vpc.tf
@@ -13,7 +13,7 @@ variable "access_token"{
 provider "google" {
   project = var.project_id
   region  = var.region
-#  access_token = var.access_token
+  #  access_token = var.access_token
 }
 
 # VPC
